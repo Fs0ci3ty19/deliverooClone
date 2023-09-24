98627dec55b3d83bae4275c64f8c452cc9f8362e
Fs0ci3ty19 <abdul.moueed@outlook.com>
2023-09-24 22:22:27 +0500

    Test commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+
+const Page = () => {
+  return <View style={{ backgroundColor: Colors.realGrey, flex: 1 }}></View>;
+};
+
+export default Page;
+
+const styles = StyleSheet.create({});
---
+++ components/CustomHeader.tsx
@@ -0,0 +1,140 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import React, { useRef } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+import { TextInput } from "react-native-gesture-handler";
+import BottomSheet from "./BottomSheet";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons
+          style={styles.searchIcon}
+          name="ios-search"
+          size={20}
+          color={Colors.medium}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Restaurants, groceries, dishes"
+        />
+      </View>
+      <Link href={"/"} asChild>
+        <TouchableOpacity style={styles.optionButton}>
+          <Ionicons name="options-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </Link>
+    </View>
+  </View>
+);
+
+const CustomHeader = () => {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+
+  const openModal = () => {
+    bottomSheetRef.current?.present();
+  };
+
+  return (
+    <SafeAreaView>
+      <BottomSheet ref={bottomSheetRef} />
+      <View style={styles.container}>
+        <TouchableOpacity onPress={openModal}>
+          <Image
+            style={styles.bike}
+            source={require("@/assets/images/bike.png")}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.titleContainer} onPress={openModal}>
+          <Text style={styles.title}>Delivery · Now</Text>
+          <View style={styles.locationName}>
+            <Text style={styles.subtitle}>Lahore</Text>
+            <Ionicons name="chevron-down" size={20} color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons name="person-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </View>
+      <SearchBar />
+    </SafeAreaView>
+  );
+};
+
+export default CustomHeader;
+
+const styles = StyleSheet.create({
+  bike: {
+    width: 30,
+    height: 30,
+  },
+  container: {
+    height: 60,
+    backgroundColor: "#fff",
+    marginTop: StatusBar.currentHeight,
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  headerContainer: { backgroundColor: Colors.lightGrey },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  locationName: { flexDirection: "row", alignItems: "center", gap: 4 },
+  searchContainer: {
+    height: 60,
+    // backgroundColor: "red",
+  },
+  searchSection: {
+    flexDirection: "row",
+    gap: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
+  input: {
+    padding: 10,
+    color: Colors.mediumDark,
+  },
+  searchIcon: {
+    paddingLeft: 10,
+  },
+});
